f95c11d804bfb083935e529885a3ad489145f343
v0agent <it+v0agent@vercel.com>
2026-07-02 16:53:46 +0000

    feat: update HeroScene component with new camera, lights, and table setup
    Co-authored-by: Zoha Fatima <204393414+ZohaNotFound@users.noreply.github.com>

--- src/components/Hero/HeroScene.jsx
+++ src/components/Hero/HeroScene.jsx
@@ -1,22 +1,39 @@
-import { useFrame } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
-import { useRef } from 'react'
-import CameraRig from './CameraRig'
-import Lights from './Lights'
-import PoolTable from './PoolTable'
-import Effects from './Effects'
-
-function easeInOutQuad(t) {
-  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
-}
 
 export default function HeroScene({ isVisible }) {
   return (
     <>
-      <CameraRig />
-      <Lights />
-      <PoolTable />
-      <Effects />
+      {/* Camera */}
+      <PerspectiveCamera makeDefault position={[0, 2.5, 4.5]} fov={50} />
+      
+      {/* Lights */}
+      <ambientLight intensity={0.2} color="#1a1510" />
+      <directionalLight 
+        position={[0, 9, 1]} 
+        intensity={2.2} 
+        color="#FFB84D"
+        castShadow
+      />
+      <spotLight 
+        position={[0.5, 8, 0.2]} 
+        angle={0.5} 
+        penumbra={0.9} 
+        intensity={150} 
+        color="#FFB84D"
+        castShadow
+      />
+      
+      {/* Table */}
+      <mesh position={[0, -0.12, 0]} receiveShadow castShadow>
+        <boxGeometry args={[3.4, 0.25, 1.9]} />
+        <meshStandardMaterial color="#2a1f14" roughness={0.4} metalness={0.05} />
+      </mesh>
+      
+      {/* Felt */}
+      <mesh position={[0, 0.02, 0]} receiveShadow castShadow>
+        <boxGeometry args={[2.84, 0.05, 1.42]} />
+        <meshStandardMaterial color="#1e5631" roughness={0.95} metalness={0} />
+      </mesh>
     </>
   )
 }
